7667a1a3da283efbecc080960ca1b5a828e3ccab
Tanishq Patil <tanishqp101204@gmail.com>
2025-12-25 05:58:05 +0000

    feat: encoding long and lat

--- src/commands/geospatial_cmds.rs
+++ src/commands/geospatial_cmds.rs
@@ -12,6 +12,32 @@ const MIN_LONGITUDE: f64 = -180.0;
 const MAX_LONGITUDE: f64 = 180.0;
 const MIN_LATITUDE: f64 = -85.05112878;
 const MAX_LATITUDE: f64 = 85.05112878;
+const LATITUDE_RANGE: f64 = MAX_LATITUDE - MIN_LATITUDE;
+const LONGITUDE_RANGE: f64 = MAX_LONGITUDE - MIN_LONGITUDE;
+
+fn spread_int32_to_int64(v: u32) -> u64 {
+    let mut result = v as u64;
+    result = (result | (result << 16)) & 0x0000FFFF0000FFFF;
+    result = (result | (result << 8)) & 0x00FF00FF00FF00FF;
+    result = (result | (result << 4)) & 0x0F0F0F0F0F0F0F0F;
+    result = (result | (result << 2)) & 0x3333333333333333;
+    (result | (result << 1)) & 0x5555555555555555
+}
+
+fn interleave(x: u32, y: u32) -> u64 {
+    let x_spread = spread_int32_to_int64(x);
+    let y_spread = spread_int32_to_int64(y);
+    let y_shifted = y_spread << 1;
+    x_spread | y_shifted
+}
+
+fn geohash_encode(latitude: f64, longitude: f64) -> u64 {
+    let normalized_latitude = 2.0_f64.powi(26) * (latitude - MIN_LATITUDE) / LATITUDE_RANGE;
+    let normalized_longitude = 2.0_f64.powi(26) * (longitude - MIN_LONGITUDE) / LONGITUDE_RANGE;
+    let lat_int = normalized_latitude as u32;
+    let lon_int = normalized_longitude as u32;
+    interleave(lat_int, lon_int)
+}
 
 pub fn cmd_geoadd(db: &Db, args: &[Value]) -> Value {
     if args.len() < 4 {
@@ -54,7 +80,8 @@ pub fn cmd_geoadd(db: &Db, args: &[Value]) -> Value {
         Ok(m) => m,
         Err(_) => return Value::Error("ERR invalid member".into()),
     };
-    let score = 0.0;
+    
+    let score = geohash_encode(latitude, longitude) as f64;
     let added = db.zadd(key, score, member);
     Value::Integer(added as i64)
 }
